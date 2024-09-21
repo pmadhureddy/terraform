798c7944331cecd780c6089f0390383edd56c57b
pmadhureddy <pmadhureddy1510@gmail.com>
2024-09-21 08:17:32 +0530

    creating instances dynamically by using data source

---
+++ data-sources/ec2.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+  name        = "allow_ssh"
+  description = "Allow port number 22 for ssh access"
+#   vpc_id      = aws_vpc.main.id
+
+
+tags = {
+    Name = "allow_ssh"
+  }
+
+
+#usually allows everything in egress
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+}
+
+
+resource "aws_instance" "terraform" {
+  ami           = data.aws_ami.learningdevops.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+
+  tags = {
+    Name = "terrafrom"
+  }
+}
